ead812ccf51694b192740b7ff9c62ba02f2afd25
faroukfaiz10 <faizfarouk2015@gmail.com>
2024-12-12 00:49:25 +0100

    day 11: insert at the end (order doesn't matter)

--- 11/a/script.py
+++ 11/a/script.py
@@ -11,18 +11,14 @@ with open("../input.txt") as file:
     stones = file.read().split()
 
 for k in range(25):
-    offset = 0
     for i in range(len(stones)):
-        idx = i + offset
-        l = len(stones[idx])
-        if stones[idx] == "0":
-            stones[idx] = "1"
+        l = len(stones[i])
+        if stones[i] == "0":
+            stones[i] = "1"
         elif l % 2 == 0:
-            right = stones[idx][l // 2 :].lstrip("0")
-            right = right if len(right) else "0"
-            stones.insert(idx + 1, right)
-            stones[idx] = stones[idx][: l // 2]
-            offset += 1
+            right = stones[i][l // 2 :].lstrip("0") or "0"
+            stones.append(right)
+            stones[i] = stones[i][: l // 2]
         else:
-            stones[idx] = str(int(stones[idx]) * 2024)
+            stones[i] = str(int(stones[i]) * 2024)
 print(len(stones))
