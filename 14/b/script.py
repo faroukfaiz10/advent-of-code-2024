c0d4417170c953dbc2dac6027c998fbcc25b0d1b
faroukfaiz10 <faizfarouk2015@gmail.com>
2024-12-14 15:42:26 +0100

    day 14

---
+++ 14/b/script.py
@@ -0,0 +1,43 @@
+import re
+from collections import Counter, defaultdict
+from itertools import zip_longest
+from operator import itemgetter
+from bisect import bisect_left, insort
+from functools import cache
+import math
+
+WIDTH = 101
+HEIGHT = 103
+
+with open("../input.txt") as file:
+    lines = file.read().splitlines()
+
+ps = []
+vs = []
+for line in lines:
+    p, v = line.split()
+    ps.append([int(i) for i in p.split("=")[1].split(",")])
+    vs.append([int(i) for i in v.split("=")[1].split(",")])
+
+
+for k in range(10000):
+    for i in range(len(ps)):
+        x, y = ps[i]
+        x = (x + vs[i][0]) % WIDTH
+        y = (y + vs[i][1]) % HEIGHT
+        ps[i] = [x, y]
+
+    if (k - 87) % 101 == 0:
+        for i in range(HEIGHT):
+            l = ["#" if [j, i] in ps else "." for j in range(WIDTH)]
+            print("".join(l))
+        print(k, "===================")
+
+quadrants = [0] * 4
+for x, y in ps:
+    if x == WIDTH // 2 or y == HEIGHT // 2:
+        continue
+    quadrants[2 * (x < WIDTH // 2) + (y < HEIGHT // 2)] += 1
+
+
+print(math.prod(quadrants))
